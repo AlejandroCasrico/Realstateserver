6a45c85d3324f173ec9badbbce598b88f2e23513
AlejandroCasrico <you@example.com>
2022-11-03 17:48:32 -0700

    servidor2

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const authController = require('../controllers/auth.controller')
+const basicAuth = require('express-basic-auth')
 /* GET home page. */
-router.get('/get-jwt', authController.generateJWT);
+router.get('/get-jwt',basicAuth({
+    users: { 'admin': 'supersecret' }
+}), authController.generateJWT);
 
 
 module.exports = router;
