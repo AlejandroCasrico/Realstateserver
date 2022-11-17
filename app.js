5f74264ceb70aa628d841c985b401abf3f8730e9
AlejandroCasrico <you@example.com>
2022-11-17 12:21:11 -0700

    error

--- app.js
+++ app.js
@@ -8,15 +8,16 @@ const mongoose = require('mongoose');
 mongoose.connection.on('open', function(){
   console.log("database connection")
 });
+var cors = require('cors')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var housesRouter = require('./routes/houses');
-var cors = require('cors')
 var app = express();
 const config = require('./config').configuration;
 const authRouter= require('./routes/auth');
 mongoose.connect(config.mongodb.url);
 // view engine setup
+app.use(cors());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -30,7 +31,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/auth',authRouter);
 app.use('/houses',housesRouter);
-app.use(cors());
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
